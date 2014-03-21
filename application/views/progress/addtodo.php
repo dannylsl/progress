e229b5946bceebeedf3490a4c1314dec6e496fe1
shaoling <shaoling.li@intel.com>
2014-03-21 17:20:26 +0800

    [BUG FIXED] $event[0] -> $event

--- application/views/progress/addtodo.php
+++ application/views/progress/addtodo.php
@@ -2,17 +2,17 @@
 if (isset($edit) && $edit) {
     $flag = True;
     echo form_open('progress/update_event');
-    echo "<input type='hidden' value='{$event[0]['id']}' name='eId' />";
+    echo "<input type='hidden' value='{$event['id']}' name='eId' />";
 } else {
     $flag = False;
     echo form_open('progress/save_event');
 }
 ?>
-<input type='text' class='dialog-input' name='title' placeholder='TITLE' <?=($flag)?"value='{$event[0]['title']}'":"" ?>  />
+<input type='text' class='dialog-input' name='title' placeholder='TITLE' <?=($flag)?"value='{$event['title']}'":"" ?>  />
 <select class='dialog-select' name='category'>
     <? foreach($categorys as $cate) :?>
         <?
-        if( $flag && ($cate['value'] == $event[0]['category'])) {
+        if( $flag && ($cate['value'] == $event['category'])) {
             echo "<option value='{$cate['value']}' selected='selected'>{$cate['value']}</option>";
         }else {
             echo "<option value='{$cate['value']}'>{$cate['value']}</option>";
@@ -22,6 +22,6 @@ if (isset($edit) && $edit) {
 
 </select>
 <div class='sp15'></div>
-<textarea class='dialog-textarea' id='jwysiwy_dialog' name='description' placeholder='CONTENT...'><?=($flag)?"{$event[0]['description']}":"" ?></textarea>
+<textarea class='dialog-textarea' id='jwysiwy_dialog' name='description' placeholder='CONTENT...'><?=($flag)?"{$event['description']}":"" ?></textarea>
 <button type='submit'>Submit</button>
 </form>
